92b9dd2cebbdc0540531bd1499317859b57df1df
Leonardo Sasseron <leo.dias360@gmail.com>
2019-04-06 19:14:16 -0300

    First

---
+++ Game/Assets/Scripts/TrocaCena.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TrocaCena : MonoBehaviour {
+	public void trocaparacena (int trocaacena) {
+		SceneManager.LoadScene (trocaacena);
+	}
+}
